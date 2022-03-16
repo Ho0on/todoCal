dc0df3e099659976455acf0047d94b9586b65f94
SangHoon <joa3668@gmail.com>
2022-03-16 17:14:59 +0900

    ✨ [Feat] 캘린더 컴포넌트 레이아웃 추가

---
+++ src/components/Portal.tsx
@@ -0,0 +1,15 @@
+import { createPortal } from 'react-dom';
+
+interface Props {
+  selector?: string;
+}
+
+const Portal: React.FC<Props> = ({ children, selector }) => {
+  const rootElement = selector && document.querySelector(selector);
+
+  return (
+    <div>{rootElement ? createPortal(children, rootElement) : children}</div>
+  );
+};
+
+export default Portal;

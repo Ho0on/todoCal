2d1561d5f88ed8ff26112efc6a2220680224d196
SangHoon <joa3668@gmail.com>
2022-03-26 13:53:45 +0900

    ✨ [Feat] 캘린더 날짜 클릭 모달 출력 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Global, css } from '@emotion/react';
+import styled from '@emotion/styled/macro';
 import { RecoilRoot } from 'recoil';
-import Calendar from './components/Calendar';
 
-const App = () => {
-  return (
-    <RecoilRoot>
+import Calendar from './features/Calendar';
+import TodoFormModal from './features/TodoFormModal';
+import TodoStatisticsModal from './features/TodoStatisticsModal';
+
+const globalStyle = css`
+  html,
+  body {
+    background-color: #19181a;
+    margin: 0;
+    font-family: sans-serif, serif, 'Apple SD Gothic Neo';
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 24px;
+`;
+
+const App: React.FC = () => (
+  <RecoilRoot>
+    <Global styles={globalStyle} />
+    <Container>
       <Calendar />
-    </RecoilRoot>
-  );
-};
+    </Container>
+    <TodoFormModal />
+    <TodoStatisticsModal />
+  </RecoilRoot>
+);
 
 export default App;
